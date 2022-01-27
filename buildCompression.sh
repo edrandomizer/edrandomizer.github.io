4b7f8a32d85c04cab84dbc63c2cbdbbeb310fb9b
edrandomizer <edrandomizer@protonmail.com>
2022-01-27 21:00:07 +0000

    Bug fixes and seeding

--- buildCompression.sh
+++ buildCompression.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-emcc SK_ASCCompression.c -s EXPORTED_FUNCTIONS=_DecompressSK_ASC,_DecompressText,_malloc,_free -s EXPORTED_RUNTIME_METHODS=ccall,getValue -s ALLOW_MEMORY_GROWTH=1 -o skasc.js -fsanitize=undefined,address
+emcc SK_ASCCompression.c -s EXPORTED_FUNCTIONS=_DecompressSK_ASC,_DecompressText,_malloc,_free -s EXPORTED_RUNTIME_METHODS=ccall,getValue -s ALLOW_MEMORY_GROWTH=1 -s WASM=0 -o skasc.js
